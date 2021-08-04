f5fa99922e1c99834a530fc908a698ec9ae99a93
Rowin206 <rowinvanamsterdam@gmail.com>
2021-08-04 14:46:02 +0200

    Added the world introduction to koios

--- src/pages/WorldOverview.tsx
+++ src/pages/WorldOverview.tsx
@@ -9,27 +9,17 @@ type CourseContentParams = {
 };
 
 const WorldOverview = () => {
-    const data = worldsData;
     const {worldContent} = useParams<CourseContentParams>();
-
-    const world = data.find((item) => Slugify(item.url, { lowerCase: true, replaceAmpersand: "and" }) === worldContent);
-
-    // const lastVersion = store.get('lastWatched');
+    const world = worldsData.find((item) => Slugify(item.url, { lowerCase: true, replaceAmpersand: "and" }) === worldContent);
 
     return (
         <div className={'container worldOverviewContainer'}>
             <div className={'worldOverviewContainer__titleRow'}>
                 <h1 className={'worldOverviewContainer__titleRow__title'}>{world?.course}</h1>
-                {/*{lastVersion &&*/}
-                {/*<div className={'worldOverviewContainer__titleRow__button'}>*/}
-                {/*    <Button title={'Continue Learning'} link={'/worlds/' + lastVersion.world + '/' + lastVersion.level + lastVersion.video}/>*/}
-                {/*</div>*/}
-                {/*}*/}
             </div>
             <p className={'worldOverviewContainer__description'}>{world?.description}</p>
             <QuickLinks data={world?.quickLinks}/>
             <div className={'linkCardContainer'}>
-                {console.log(world.content)}
                 <LinkCard data={world?.content} baseUrl={worldContent} title={'Learn'}/>
                 <LinkCard data={world?.earn} baseUrl={worldContent} title={'Earn'}/>
                 <LinkCard data={world?.connect} baseUrl={worldContent} title={'Connect'}/>
@@ -37,5 +27,4 @@ const WorldOverview = () => {
         </div>
     )
 }
-
 export default WorldOverview;
