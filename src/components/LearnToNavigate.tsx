c148ca399b771b47906fcdc9bd2fb218f7e7642a
Stephan Kop <stephan.kop@outlook.com>
2021-08-11 18:09:18 +0200

    header finished and learn to navigate almost done

---
+++ src/components/LearnToNavigate.tsx
@@ -0,0 +1,30 @@
+import data from '../assets/data/learnToNavigate.json'
+import QuickLinks from "./QuickLinks";
+import Button from "./Button";
+import store from "store";
+
+const LearnToNavigate = (props) => {
+  const continueLearning = store.get('lastWatched');
+
+  return (
+    <div className={'learnToNavigate-container'}>
+      <div className={'learnToNavigate'}>
+        <h2 className={'learnToNavigate__title'}>{data.title}</h2>
+        <p className={'learnToNavigate__description'}>{data.description}</p>
+        <div className={'links'}>
+          {continueLearning &&
+          <div className={'continue'}>
+            <a href={'/worlds/' + continueLearning.world + '/' + continueLearning.level + continueLearning.video}>Continue learning</a>
+          </div>
+          }
+          <QuickLinks data={props.world?.quickLinks}/>
+        </div>
+      </div>
+      <div className={'image'}>
+        <img src={'/images/artwork_blockchain.png'}/>
+      </div>
+    </div>
+  )
+}
+
+export default LearnToNavigate;
--- src/pages/WorldOverview.tsx
+++ src/pages/WorldOverview.tsx
@@ -2,29 +2,41 @@ import worldsData from '../assets/data/Worlds.json';
 import {useParams} from "react-router-dom";
 import QuickLinks from "../components/QuickLinks";
 import LinkCard from "../components/LinkCard";
-import { Slugify } from '../components/Util/Slugify';
+import {Slugify} from '../components/Util/Slugify';
+import SmallCard from "../components/SmallCard";
+import LearnToNavigate from "../components/LearnToNavigate";
 
 type CourseContentParams = {
-    worldContent: string;
+  worldContent: string;
 };
 
 const WorldOverview = () => {
-    const {worldContent} = useParams<CourseContentParams>();
-    const world = worldsData.find((item) => Slugify(item.url, { lowerCase: true, replaceAmpersand: "and" }) === worldContent);
+  const {worldContent} = useParams<CourseContentParams>();
+  const world = worldsData.find((item) => Slugify(item.url, {
+    lowerCase: true,
+    replaceAmpersand: "and"
+  }) === worldContent);
 
-    return (
-        <div className={'container worldOverviewContainer'}>
-            <div className={'worldOverviewContainer__titleRow'}>
-                <h1 className={'worldOverviewContainer__titleRow__title'}>{world?.course}</h1>
-            </div>
-            <p className={'worldOverviewContainer__description'}>{world?.description}</p>
-            <QuickLinks data={world?.quickLinks}/>
-            <div className={'linkCardContainer'}>
-                <LinkCard data={world?.content} baseUrl={worldContent} title={'Learn'}/>
-                <LinkCard data={world?.earn} baseUrl={worldContent} title={'Earn'}/>
-                <LinkCard data={world?.connect} baseUrl={worldContent} title={'Connect'}/>
-            </div>
+  return (
+    <>
+      <div className={'worldOverviewHeader'}>
+        <div className={'worldOverviewHeader__titleRow'}>
+          <h1 className={'worldOverviewHeader__titleRow__title'}>{world?.course}</h1>
+          <p className={'worldOverviewHeader__titleRow__description'}>{world?.description}</p>
         </div>
-    )
+        <SmallCard/>
+      </div>
+      <div className={'container worldOverviewContainer'}>
+        <LearnToNavigate world={world}/>
+        {/*<QuickLinks data={world?.quickLinks}/>*/}
+        {/*<div className={'linkCardContainer'}>*/}
+        {/*  <LinkCard data={world?.content} baseUrl={worldContent} title={'Learn'}/>*/}
+        {/*  <LinkCard data={world?.earn} baseUrl={worldContent} title={'Earn'}/>*/}
+        {/*  <LinkCard data={world?.connect} baseUrl={worldContent} title={'Connect'}/>*/}
+        {/*</div>*/}
+      </div>
+    </>
+  )
 }
 export default WorldOverview;
+
