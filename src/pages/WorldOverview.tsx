a3ba075ba26ad0964c0ae5743e03c3013410017d
Stephan Kop <stephan.kop@outlook.com>
2021-08-18 17:43:01 +0200

    finished except for quicklinks

--- src/pages/WorldOverview.tsx
+++ src/pages/WorldOverview.tsx
@@ -1,12 +1,17 @@
 import worldsData from '../assets/data/Worlds.json';
-import {useParams} from "react-router-dom";
+import {useParams, Link} from "react-router-dom";
 import QuickLinks from "../components/QuickLinks";
 import LinkCard from "../components/LinkCard";
+import DiscordEmbed from '../components/discordEmbed';
 import {Slugify} from '../components/Util/Slugify';
 import SmallCard from "../components/SmallCard";
 import LearnToNavigate from "../components/LearnToNavigate";
 import LevelCards from "../components/LevelCards";
 import Team from "../components/Team";
+import {useRef} from "react";
+import cardData from "../assets/data/overviewHeaderLinks.json";
+import {Icon} from "../components/Util/Icon";
+import {SvgSprite} from "../components/Util/SvgSprite";
 
 type CourseContentParams = {
   worldContent: string;
@@ -19,6 +24,9 @@ const WorldOverview = () => {
     replaceAmpersand: "and"
   }) === worldContent);
 
+  const learn = useRef(null);
+  const team = useRef(null);
+
   return (
     <>
       <div className={'worldOverviewHeader'}>
@@ -26,12 +34,40 @@ const WorldOverview = () => {
           <h1 className={'worldOverviewHeader__titleRow__title'}>{world?.course}</h1>
           <p className={'worldOverviewHeader__titleRow__description'}>{world?.description}</p>
         </div>
-        <SmallCard/>
+        {/*<SmallCard/>*/}
+        <div className={"smallCard-container"}>
+          {cardData.map((data) => {
+            return (
+              <div className={"smallCard"}>
+                <div className={"smallCard__title-row"}>
+                  <h2 className={"smallCard__title"}>{data.title}</h2>
+                  <Icon type={data.icon as keyof typeof SvgSprite}/>
+                </div>
+                <p className={"smallCard__description"}>{data.description}</p>
+                {data.title === 'Learn' &&
+                  <button className={"smallCard__link"} onClick={() => learn.current.scrollIntoView({behavior: 'smooth'})}>{data.linkTitle}</button>
+                }
+                {data.title === 'Earn' &&
+                  <Link className={"smallCard__link"} to={'/earn'}>{data.linkTitle}</Link>
+                }
+                {data.title === 'Connect' &&
+                  <button className={"smallCard__link"} onClick={() => team.current.scrollIntoView({behavior: 'smooth'})}>{data.linkTitle}</button>
+                }
+              </div>
+            )
+          })
+          }
+        </div>
       </div>
       <div className={'container worldOverviewContainer'}>
-        <LearnToNavigate world={world}/>
-        <LevelCards world={world} baseUrl={worldContent}/>
-        <Team world={world}/>
+        <div ref={learn}>
+          <LearnToNavigate world={world}/>
+          <LevelCards world={world} baseUrl={worldContent}/>
+        </div>
+        <DiscordEmbed/>
+        <div ref={team}>
+          <Team world={world}/>
+        </div>
         {/*<QuickLinks data={world?.quickLinks}/>*/}
         {/*<div className={'linkCardContainer'}>*/}
         {/*  <LinkCard data={world?.content} baseUrl={worldContent} title={'Learn'}/>*/}
