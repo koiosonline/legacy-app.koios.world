ba66d7083b281962d8a24f56e0100e2c707391ed
Rowin206 <rowinvanamsterdam@gmail.com>
2021-08-25 17:41:25 +0200

    fix continue learning on overview page

--- src/components/LearnToNavigate.tsx
+++ src/components/LearnToNavigate.tsx
@@ -14,7 +14,7 @@ const LearnToNavigate = (props) => {
         <div className={'links'}>
           {continueLearning &&
           <div className={'continue'}>
-            <a href={'/worlds/' + continueLearning.world + '/' + continueLearning.level + continueLearning.video}>Continue learning</a>
+            <a href={'#/worlds/' + continueLearning.world + '/' + continueLearning.level + continueLearning.video}>Continue learning</a>
           </div>
           }
           <QuickLinks data={props.world?.quickLinks}/>
