8eb201ca7d12a4dd608d52cbe65b7f0c74296d60
Stephan Kop <stephan.kop@outlook.com>
2021-09-01 17:20:00 +0200

    fixed comments

--- src/components/Web3/ConnectionCheck.tsx
+++ src/components/Web3/ConnectionCheck.tsx
@@ -18,12 +18,10 @@ import {
 
 export const Connect = async () => {
   await connectWeb3();
-  // console.log("connected");
   return provider;
 };
 
 export const Disconnect = async () => {
   await disconnectWeb3();
-  // console.log("disconnected");
   return provider;
 };
