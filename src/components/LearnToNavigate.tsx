a1138b66f546cc3f110fd72c2bc1a08b5ee4e856
Rowin206 <rowin.amsterdam@gmail.com>
2022-02-15 08:04:50 +0100

    Removed console log

--- src/components/LearnToNavigate.tsx
+++ src/components/LearnToNavigate.tsx
@@ -4,7 +4,6 @@ import store from "store";
 
 const LearnToNavigate = (props) => {
   const continueLearning = store.get("lastWatched");
-  console.log(props);
 
   return (
     <div className={"learnToNavigate-container"}>
