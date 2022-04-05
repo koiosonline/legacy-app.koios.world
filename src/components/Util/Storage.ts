a08c6c9dc4719307251a13cac5fd9781275d590c
duart38 <duartsnel@icloud.com>
2022-04-05 13:15:17 +0200

    add jsdoc for store and getJson

--- src/components/Util/Storage.ts
+++ src/components/Util/Storage.ts
@@ -1,4 +1,12 @@
+/**
+ * Helper object containing methods to interact with localStorage more easily.
+ */
 export const store = {
+  /**
+   * 
+   * @param key name of the localStorage item
+   * @returns a JSON parsed from the string within the localStorage key provided or undefined.
+   */
   getJson: <T extends Record<string, unknown>>(key: string): T => JSON.parse(localStorage.getItem(key)),
   remove: (key: string) => localStorage.removeItem(key),
   setJson: (key: string, data: Record<string, any>) => localStorage.setItem(key, JSON.stringify(data))
