87c652cf0abe304005d35acc7129039656f3959c
duart38 <duartsnel@icloud.com>
2022-04-13 15:16:36 +0200

    add cache fallback

--- public/caching.js
+++ public/caching.js
@@ -1,6 +1,14 @@
 const cacheName = 'v1';
 const console = this.console;
 
+const CACHE_FLAGS = [
+  /http(s|):\/\/.+\.(svg|png|PNG|jpg|jpeg|ico|md|ttf)/g, // files we can cache and forget
+  /http(s|):\/\/.+manifest\.json/g // just the manifest file. other JSON files might be updated too frequently
+];
+
+function cachable(url){
+  return CACHE_FLAGS.some(r=>r.test(url)); // TODO: can we shrink?
+}
 
 this.addEventListener('install', e => {
   console.log('Service Worker: Installed');
@@ -25,18 +33,33 @@ this.addEventListener('activate', e => {
 
 // fetch used.
 this.addEventListener('fetch', e => {
-  console.log('Service Worker: Fetching', e);
+  // storing non GET reqs not 'really' supported.
+  if (e.request.method != "GET") return;
+
+  console.log(`Service worker fetch for (${e.request.url})`);
   e.respondWith(
-    this.fetch(e.request)
-      .then(res => {
-        if(e.request.method === "GET"){
-          const responseClone = res.clone();
-          this.caches.open(cacheName).then(cache => {
-            cache.put(e.request, responseClone);
-          });
-        }
-        return res;
-      })
-      .catch(() => this.caches.match(e.request).then(res => res))
+    (async function () {
+      // Try to get the response from a cache.
+      const cache = await this.caches.open(cacheName);
+      const cachedResponse = await cache.match(e.request);
+
+      if (cachedResponse) {
+        e.waitUntil(cache.add(e.request));
+        return cachedResponse;
+      }else{
+        return await this.fetch(e.request).then(res => {
+          if(cachable(e.request.url)){
+            console.log(`Stored result of ${e.request.url}`);
+            const responseClone = res.clone();
+            this.caches.open(cacheName).then(cache => {
+              cache.put(e.request, responseClone);
+            });
+          } else {
+            console.log(`NOT STORING... url (${e.request.url})`);
+          }
+          return res;
+        });
+      }
+    })()
   );
 });
