fd28305db0f7afdfafd85592147d0c8560d030f1
duart38 <duartsnel@icloud.com>
2022-06-08 09:55:46 +0200

    Changed tokenAddress var to use env

--- src/components/Web3/AddTokenToWallet.ts
+++ src/components/Web3/AddTokenToWallet.ts
@@ -1,5 +1,5 @@
 export async function AddTokenToWallet(provider) {
-  const tokenAddress = '0xB49750AD82d11C12209A837210AB753AB09115a7';
+  const tokenAddress = process.env.REACT_APP_TITAN_COIN_CONTRACT_ADDRESS_POLYGON;
   const tokenSymbol = 'Titan';
   const tokenDecimals = 18;
   const tokenImage = 'https://raw.githubusercontent.com/koiosonline/app.koios.world/main/public/images/koios-icon.svg';
